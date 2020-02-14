a3007bd045df8b5e52e97ea613fd3dfd8a16b514
lowem <lowem@otc.edu>
2020-02-14 10:12:26 -0600

    Initial UI Commit

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Scene curScene;
+
+    // Use this for initialization
+    void Start(int level)
+    {
+        SceneManager.LoadScene("Level_" + level);
+        SceneManager.UnloadSceneAsync(curScene);
+    }
+}
