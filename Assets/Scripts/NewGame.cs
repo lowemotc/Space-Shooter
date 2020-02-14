58c1f71459c04876b7e710ebbd485f454981a812
lowem <lowem@otc.edu>
2020-02-14 10:22:42 -0600

    WIP

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+    public Scene curScene;
+    
+    public void NewGameButton()
+    {
+        SceneManager.LoadScene("Level_1");
+        SceneManager.UnloadSceneAsync(curScene);
+    }
+}
