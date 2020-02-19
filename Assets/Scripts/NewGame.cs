a79c3713f3aec718084160b748471e4ebcf61442
lowem <lowem@otc.edu>
2020-02-19 13:36:29 -0600

    Main Menu Prototype

--- Assets/Scripts/NewGame.cs
+++ Assets/Scripts/NewGame.cs
@@ -3,11 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class NewGame : MonoBehaviour
 {
-    public Scene curScene;
-    
+
     public void NewGameButton()
     {
         SceneManager.LoadScene("Level_1");
-        SceneManager.UnloadSceneAsync(curScene);
     }
 }
